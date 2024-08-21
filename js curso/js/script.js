a8dc2771a2645503ce716a2bb96a896e98c12e39
ItsNachoGG <newprofilekpa@gmail.com>
2024-08-20 21:08:13 -0300

    subiendo primer trabajo

---
+++ js curso/js/script.js
@@ -0,0 +1,17 @@
+let numeroA = 1;
+let numeroB = 2;
+const NUMEROC = 3;
+
+let suma = numeroA + numeroB;
+
+console.log("la suma es " + suma); 
+
+let producto = numeroB * NUMEROC;
+console.log("el producto es " + producto);
+
+let producto2 = numeroA / numeroB;
+console.log("el producto es " + producto2);
+
+let producto3 = numeroB - numeroA; 
+
+console.log("el producto es " + producto3);
