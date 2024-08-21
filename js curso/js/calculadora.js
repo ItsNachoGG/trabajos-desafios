0a44ca49cfaa3df9916fcc71aa32a8d48c13d140
ItsNachoGG <newprofilekpa@gmail.com>
2024-08-20 21:48:46 -0300

    actualizando script

--- js curso/js/calculadora.js
+++ js curso/js/calculadora.js
@@ -5,22 +5,22 @@ let sumRes = numeroA + numeroB;
 
 alert("la suma es: " + sumRes);
 
-let numRes1 = parseInt(prompt("ingrese primer valor"));
-let numRes2 = parseInt(prompt("ingrese segundo valor"));
+let numRes1 = parseInt(prompt("ingrese primer valor a restar"));
+let numRes2 = parseInt(prompt("ingrese segundo valor a restar"));
 
 let resRes = numRes1 - numRes2;
 
 alert("la resta es: " + resRes);
 
-let divNum1 = parseInt(prompt("ingrese primer valor"));
-let divNum2 = parseInt(prompt("ingrese segundo valor"));
+let divNum1 = parseInt(prompt("ingrese primer valor a dividir"));
+let divNum2 = parseInt(prompt("ingrese segundo valor a dividir"));
 
 let divRes = divNum1 / divNum2;
 
 alert("la division es: " + divRes);
 
-let multNum1 = parseInt(prompt("ingrese primer valor"));
-let multNum2 = parseInt(prompt("ingrese segundo valor"));
+let multNum1 = parseInt(prompt("ingrese primer valor a multiplicar"));
+let multNum2 = parseInt(prompt("ingrese segundo valor a multiplicar"));
 
 let multRes = multNum1 * multNum2;
 
